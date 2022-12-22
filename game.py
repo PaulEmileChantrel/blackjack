39a13f7d7fbf6eb0f79231b1d210d8278ba4a4d0
PaulEmileChantrel <paul-emile.chantrel.1@ulaval.ca>
2022-12-22 10:57:51 +0100

    bug fix

--- game.py
+++ game.py
@@ -1,10 +1,11 @@
 from player import Dealer, HumanGambler, RandomComputerGambler
-
+import random
+import time
 class CardGame:
     def __init__(self):
         self.deck = self.create_cards()
-        random.suffle(self.deck)
-        self.card_out = []
+        self.shuffle_deck()
+        self.card_out = [] #card tossed out of the deck
 
     def create_cards(self):
         # [1-13,'H'-'S'-'D'-'C'] ex. [2,'D']
@@ -20,34 +21,135 @@ class CardGame:
         #we always have more than one card
         self.card_out += cards
 
-    def suffle_deck(self):
-        random.suffle(self.deck)
+    def shuffle_deck(self):
+        random.shuffle(self.deck)
+
+    def print_deck(self):
+        deck_str = [str(card[0])+card[1] for card in self.deck]
+        print(f'The deck has {len(deck_str)} cards left')
+        print(deck_str)
 
     def draw_card(self):
-        if len(self.deck)<1:
+        if len(self.deck)>1:
             card = self.deck.pop(0)
             #self.card_out.append(card)
             return card
         else:#no more cards in the deck
-            self.deck, = self.card,[]
-            self.suffle_deck(self.deck)
+            self.deck,self.card_out = self.card_out,[]
+            self.shuffle_deck()
             return self.draw_card()
 
     def reinitialize_deck(self):
         self.deck = self.deck+self.card_out
         self.card = []
-        self.suffle_deck(self.deck)
-
-class BlackJack(CardGame):
-    def __init__(self):
-        super().__init__()
+        self.shuffle_deck()
 
 
-def play(game,dealer,players):
+
+def table_play(game,dealer,players):
+    i = 0
+    while player:
+        print(f'Starting a new turn:')
+        turn(game,dealer,players)
+
+
+def turn(game,dealer,players):
+    # Choose bet size
+    for player in players:
+        player.choose_bet_size()
+
+    #The dealer draw his cards
     dealer.draw_first_cards(game)
+    time.sleep(0.8)
+    #The players draw their cards
     for player in players:
         player.draw_first_cards(game)
 
+    # Check for dealer Blackjack
+    dealer_blackjack = dealer.hands[0].blackjack()
+
+    # Check for Blackjack
     for player in players:
-        print(f'Player {player.id} turn :')
+        if player.hands[0].blackjack():
+            if dealer_blackjack:
+                player.equal_hand()
+            else:
+                player.won_blackjack()
+            player.hands[0].toss_hand(game)
+
+    #No insurance concept for now
+    if dealer_blackjack:
+        for player in players:
+            if player.hands[0].status != 'done':
+                player.lost_hand()
+                player.hands[0].toss_hand(game)
+        #The turn is over
+        return
+
+
+    # Here, the dealer does not have a blackjack
+    # And the remaining player neither
+    for player in players:
+        print(f'Player {player.player_id} turn:')
+        time.sleep(0.8)
         player.get_moves(game)
+
+    # The dealer turn
+    print('The Dealer is playing:')
+    time.sleep(0.8)
+    dealer_cards = dealer.hands[0].get_printable_card()
+    print(f'The dealer cards are [{dealer_cards[0]}] and [{dealer_cards[1]}]')
+    print(f'The Dealer score is {dealer.hands[0].hand_score()}')
+    dealer.make_move(game)
+
+
+    if dealer.status =='blown_up':
+        #every remaining hands won
+        for player in players:
+            for hand in player.hands:
+                player.won_hand()
+                hand.toss_hand(game)
+
+
+    else:
+        # hands with higher score won, lower score lose, same score equalized
+        dealer_score = dealer.score
+
+        for player in players:
+            for hand in player.hands:
+                hand_score = hand.score
+
+                if 21>=hand_score > dealer_score:
+                    player.won_hand()
+                elif hand_score < dealer_score or hand_score>21:
+                    player.lost_hand()
+                else:
+                    player.equal_hand()
+
+                hand.toss_hand(game)
+            player.bet = 0
+
+    #Print player pot
+    pot_str = "The player pots are : "
+    for player in players:
+        pot_str +=f"{player.pot} for {player.player_id}, "
+    pot_str = pot_str[:-3]
+    print(pot_str)
+    #Remove player with 0$ pot
+    for player in players:
+        if player.pot<=0:
+            print(f'Player {player.player_id} is eliminated')
+            players.remove(player)
+    # End of turn
+    return
+
+
+if __name__=='__main__':
+
+
+    game = CardGame()
+    game.print_deck()
+
+    dealer = Dealer()
+    player = [HumanGambler(10000)]
+    table_play(game,dealer,player)
--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import math
-
-
+import random
+import time
 class Hand:
     def __init__(self,game,card=None):
         if card :
@@ -8,16 +8,21 @@ class Hand:
         else:
             self.hand = [game.draw_card(),game.draw_card()]
         self.status = 'playing'
-        self.score = game.hand_score(self.hand)
+        self.score = self.hand_score()
 
     def toss_hand(self,game):
-        game.toss_card(hand)
-        self.__del__()
+        game.toss_card(self.hand)
+        del self
 
+    def __del__(self):
+        pass
+        #print("Object deleted")
 
     def blackjack(self):
         # return True if the hand is a blackjack
-        return len(self.hand) == 2 and ((self.hand[0][0]>=10 and self.hand[1][0]==1) or (self.hand[1][0]>=10 and self.hand[0][0]==1))
+        if len(self.hand) == 2 and ((self.hand[0][0]>=10 and self.hand[1][0]==1) or (self.hand[1][0]>=10 and self.hand[0][0]==1)):
+            print('BLACKJACK!')
+            return True
 
     def get_printable_card(self):
         card_str = [str(card[0])+card[1] for card in self.hand]
@@ -30,23 +35,30 @@ class Hand:
 
     def hand_score(self):
         self.score = 0
-        as = 0
+        as_num = 0
+        #print(self.hand)
         for card in self.hand:
 
             if card[0] == 1:
-                as +=1
+                as_num +=1
             else:
                 self.score += card[0] if card[0]<=10 else 10
-        if as:
-            self.score += as-1 #if only 1 as -> +0
+        if as_num:
+            self.score += as_num-1 #if only 1 as -> +0
             self.score += 11 if self.score <=10 else 1
 
         return self.score
 
     def add_card(self,game):
         # draw a card and update hand score and status
-        self.hand.append(game.draw_card())
+        card = game.draw_card()
+        self.print_hand()
+        self.hand.append(card)
+        print(f'The card [{str(card[0])+card[1]}] is drawn.')
+        self.print_hand()
         self.score = self.hand_score()
+        print(f'The new hand score is {self.score}.')
+
         if self.score >21:
             self.status = 'blown_up'
         elif self.score == 21:
@@ -71,12 +83,28 @@ class Player:
     def draw_first_cards(self,game):
         self.hands.append(Hand(game))
 
+
+
     def get_moves(self,game):
         # the current score is lower than 21
         moves = []
         for hand in self.hands:
-            move = self.get_move(game,hand)
+            if hand.score<21:
+                move = self.get_move(game,hand)
 
+
+    def won_hand(self):
+        self.pot += 2*self.bet
+
+    def lost_hand(self):
+        pass
+
+    def won_blackjack(self):
+        self.pot += 2.5*self.bet
+
+
+    def equal_hand(self):
+        self.pot += self.bet
 
 
     def make_move(self,game,hand,move):
@@ -86,8 +114,13 @@ class Player:
             hand.add_card(game)
 
             if hand.score>21:
+                self.lost_hand()
                 hand.toss_hand(game)
-            self.get_move(game,hand)
+            elif hand.score==21:
+                #end of hand turn
+                pass
+            else:
+                self.get_move(game,hand)
         elif move == 'SP':
             hands = hand.split()
             self.hands.remove(hand)
@@ -95,6 +128,7 @@ class Player:
             self.get_move(game,hands[0])
             self.get_move(game,hands[1])
         elif move == 'D':
+            #end of hand turn
             hand.status = 'done'
 
 
@@ -108,13 +142,14 @@ class Dealer(Player):
     def draw_first_cards(self,game):
         super().draw_first_cards(game)
         hand = self.hands[0].get_printable_card()
-        print(f'Dealer draw {hand[0]} and [?,?]')
+        print(f'Dealer draw [{hand[0]}] and [?]')
 
 
     def make_move(self,game):
         hand = self.hands[0]
         hand.hand_score()
         while hand.score<17:
+            time.sleep(0.5)
             hand.add_card(game)
 
         self.update_final_status()
@@ -135,26 +170,39 @@ class HumanGambler(Player):
         Player.player_number += 1
         self.player_id = Player.player_number
 
+    def choose_bet_size(self):
+        self.bet = input(f'Player {self.player_id} must choose his bet size (1-{self.pot}):')
+
+        try:
+            self.bet = int(self.bet)
+            if not 0<self.bet<=self.pot:
+                raise ValueError
+        except ValueError:
+            print(f'Wrong input, the bet size must be between 1 and {self.pot}')
+            self.choose_bet_size()
+
+        self.pot -= self.bet
 
     def draw_first_cards(self,game):
         super().draw_first_cards(game)
         hand = self.hands[0].get_printable_card()
-        print(f'Player {self.player_id} draw {hand[0]} and {hand[1]}')
+        print(f'Player {self.player_id} draw [{hand[0]}] and [{hand[1]}]')
+        print(f'The hand score is {self.hands[0].score}')
 
-    def get_move(game,hand):
-        if hand.can_split():
+    def get_move(self,game,hand):
+        if hand.can_split() and self.pot>=self.bet:
             valid_moves = ['S','D','SP']
-            move = input(f'Do you want to draw (D), stop (S) or split (SP)')
+            move = input(f'Do you want to draw (D), stop (S) or split (SP): ')
             while not move in valid_moves:
-                print('This move is incorrect. You need to type : D,S or SP')
-                move = input(f'Do you want to draw (D), stop (S) or split (SP)')
+                print('This move is incorrect. You need to type : D,S or SP.')
+                move = input(f'Do you want to draw (D), stop (S) or split (SP): ')
 
         else:
             valid_moves = ['S','D']
-            move = input(f'Do you want to draw (D) or stop (S)')
+            move = input(f'Do you want to draw (D) or stop (S): ')
             while not move in valid_moves:
-                print('This move is incorrect. You need to type : D or S')
-                move = input(f'Do you want to draw (D) or stop (S)')
+                print('This move is incorrect. You need to type : D or S.')
+                move = input(f'Do you want to draw (D) or stop (S): ')
         self.make_move(game,hand,move)
 
 class RandomComputerGambler(Player):
@@ -164,13 +212,18 @@ class RandomComputerGambler(Player):
         Player.player_number += 1
         self.player_id = Player.player_number
 
+    def choose_bet_size(self):
+        self.bet = random.randint(1,self.pot)
+        self.pot -= self.bet
+        print(f'Random Computer player bet {self.bet}$')
 
     def draw_first_cards(self,game):
         super().draw_first_cards(game)
         hand = self.hands[0].get_printable_card()
-        print(f'Player {self.player_id} draw {hand[0]} and {hand[1]}')
+        print(f'Player {self.player_id} draw [{hand[0]}] and [{hand[1]}]')
+        print(f'The hand score is {self.hands[0].score}')
 
-    def get_move(game,hand):
+    def get_move(self,game,hand):
         if hand.can_split():
             valid_moves = ['S','D','SP']
         else:
